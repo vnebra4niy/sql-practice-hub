6bf250faa157ba978b6e3b6870cac7d0a85f834f
vnebra4niy <nikitakucaevic@gmail.com>
2026-05-21 23:17:16 +0200

    AdventureWorks

---
+++ queries/AdventureWorks/queries.sql
@@ -0,0 +1,25 @@
+--1. Wyświetlić nazwy produktów, dla których są zdefiniowane poszczególne elementy:
+--a. Kolor
+
+--b. Waga
+
+--c. Rozmiar
+
+--2. Wyświetlić nazwę najlżejszego i najcięższego produktu zawierającego w nazwie „Road”.
+
+--3. Wyświetlić produkty, których cena jest wyższa od średniej.
+
+--4. Wyświetlić średnią cenę produktu w zależności od kategorii (pole [Production].[ProductCategory]).
+--Do wykonania poprawnego złączenia wykorzystać tabelę ProductSubcategory.
+
+--5. Wyświetlić imiona i nazwiska wszystkich klientów wraz z łączną sumą dokonanych przez nich
+--zakupów (do określenia sumy użyć kolumny SalesOrderHeader.SubTotal). Lista ma być posortowana
+--malejąco względem łącznej sumy zakupów.
+
+--6. Wyświetlić imiona i nazwiska wszystkich sprzedawców wraz z łączną sumą dokonanych przez nich
+--sprzedaży (do określenia sumy użyć kolumny SalesOrderHeader.SubTotal). Lista ma być posortowana
+--malejąco względem łącznej sumy sprzedaży.
+
+--7. Wyświetlić kategorie, podkategorie, imiona i nazwiska sprzedawców oraz średnią zniżkę na
+--produkty (względem pod kategorii) jakiej udzielił klientom sprzedawca. Wyświetlić tylko i wyłącznie
+--te wiersze w których średnia zniżka jest większa od zera
